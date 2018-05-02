e607021a2c22a3547d78a227a0c0d7c8b46bb465
unknown <>
2018-05-02 16:25:51 +0800

    h5小案例

---
+++ upload/upload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/04/03
+ * Time: 21:55:55
+ */
+    move_uploaded_file($_FILES['abc']['tmp_name'],'upload/'.$_FILES['abc']['name']);
+?>
